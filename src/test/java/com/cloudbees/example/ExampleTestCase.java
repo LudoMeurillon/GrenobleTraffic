6361dfd49548fa9b3af7095f14ea9ba6b5c2c126
CloudBees <no-reply@cloudbees.com>
2012-10-07 20:52:09 +0000

    Initial checkin

---
+++ src/test/java/com/cloudbees/example/ExampleTestCase.java
@@ -0,0 +1,23 @@
+package com.cloudbees.example;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+/**
+ * A sample test, so that we can have some Junit results to report.
+ * @author rcampbell
+ *
+ */
+public class ExampleTestCase {
+
+    @Test
+    public void exampleTest() {
+        Assert.assertTrue("Yay!", true);
+    }
+
+    @Test
+    public void anotherExample() {
+        Assert.assertTrue("Yay!", true);
+    }
+}
