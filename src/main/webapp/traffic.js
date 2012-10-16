41815f8e5c0e63834c71c5b0598c590dfa14bb36
Ludovic Meurillon <faustinemeurillon@MacBook-Pro-de-Faustine-Meurillon.local>
2012-10-16 21:34:21 +0200

    Fix issue #2 https://github.com/LudoMeurillon/GrenobleTraffic/issues/2

--- src/main/webapp/traffic.js
+++ src/main/webapp/traffic.js
@@ -1,13 +1,14 @@
 var map;
 var refreshDiv;
+var switchThemeDiv;
 var version = 1;
 var polylines = {}; 
+var currentTheme = "b&w";
 
 function refreshTrafficStatus(trafficData){
 	replaceText(trafficData.date);
 	trafficData.features.forEach(function(feature){
 		var color="#045FB4";
-		var lineSymbol;
 		var zIndex;
 		switch(feature.properties.NSV_ID){
 		case 1 :
@@ -27,36 +28,25 @@ function refreshTrafficStatus(trafficData){
 			zIndex = 10;
 			break;
 		}
-		drawTroncon(feature.properties.CODE,color, 0.7, 4, lineSymbol, zIndex);
+		drawTroncon(feature.properties.CODE,color, 0.7, 4, zIndex);
 	});
 	if(refreshDiv){
 		refreshDiv.className='gmap-control';
 	}
 }
 
-function drawTroncon(code, color, opacity, weight, lineSymbol, lineZIndex){
+function drawTroncon(code, color, opacity, weight, lineZIndex){
 	var coords = loadCoords(code);
-	
-	if(lineSymbol){
-		var result = new google.maps.Polyline({
-			path : coords,
-			strokeColor : color,
-			strokeOpacity : opacity,
-			strokeWeight : weight,
-			zIndex : lineZIndex,
-			icons: [{icon: lineSymbol, offset: '100%'}]
-		});
-	}else{
-		var result = new google.maps.Polyline({
-			path : coords,
-			strokeColor : color,
-			strokeOpacity : opacity,
-			strokeWeight : weight,
-			zIndex : lineZIndex,
-			strokeWeight : weight
-		});
-	}
+	var result = new google.maps.Polyline({
+		path : coords,
+		strokeColor : color,
+		strokeOpacity : opacity,
+		strokeWeight : weight,
+		zIndex : lineZIndex,
+		strokeWeight : weight
+	});
 	if(polylines[code]){
+		//remove polyline from google map
 		polylines[code].setMap(null);
 	}
 	polylines[code] = result;
@@ -70,8 +60,7 @@ function fetchTrafficInfos(){
 			if (xhr.status == 200) {
 				refreshTrafficStatus(JSON.parse(xhr.responseText));
 			} else {
-				window.alert("Error: returned status code " + xhr.status
-						+ " " + xhr.statusText);
+				console.log("Error: returned status code " + xhr.status + " " + xhr.statusText);
 			}
 		}
 	};
@@ -79,32 +68,25 @@ function fetchTrafficInfos(){
 	xhr.send(null);
 }
 
-function storeCoord(code, value){
-	localStorage[code]=JSON.stringify(value);
-}
-
 function loadCoords(code){
-	var array = [];
+	var points = [];
 	var i =0;
 	var coordinatesAsString = localStorage[code];
 	if(coordinatesAsString){
 		var coordinates = JSON.parse(coordinatesAsString);
 		for(var i=0; i<coordinates.length; i++) {
 			var value = coordinates[i];
-			array.push(new google.maps.LatLng(value[1], value[0]));
+			points.push(new google.maps.LatLng(value[1], value[0]));
 			i++;
 		}
 	}
-	return array;
+	return points;
 }
 
 function storeTroncon(troncon){
 	var code = troncon.properties.CODE;
 	var coordinates = troncon.geometry.coordinates[0];
-	
-	for(var i=0; i<coordinates.length; i++) {
-		storeCoord(code, coordinates);
-	}
+	localStorage[code]=JSON.stringify(coordinates);
 }
 
 function refreshTroncons(troncons) {
@@ -153,6 +135,20 @@ function replaceText(text){
 	map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(badge);
 }
 
+function switchTheme(){
+	var saturationValue = -100;
+	switch(currentTheme){
+	case "b&w":
+		saturationValue = 0;
+		currentTheme = "color";
+		break;
+	default:
+		currentTheme = "b&w";	
+		switchThemeDiv.className= 'gmap-control';
+	}
+	map.setOptions({styles : [{stylers: [{saturation: saturationValue }]}]});
+}
+
 function initialize() {
 	var mapOptions = {
 		center : new google.maps.LatLng(45.182037,5.727654),
@@ -177,9 +173,8 @@ function initialize() {
 	    ]};
 
 	map = new google.maps.Map(document.getElementById("mapCanvas"), mapOptions);
-	refreshDiv = addControl("Refresh",function(){
-		fetchTrafficInfos();
-	});
+	refreshDiv 		= addControl("Refresh",fetchTrafficInfos);
+	switchThemeDiv 	= addControl("Color", switchTheme);
 	
 	if(navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(setPosition, onPositionError);
@@ -211,19 +206,19 @@ function setPosition(position){
 }
 
 function onPositionError(error) {
-    var info = "Erreur lors de la gŽolocalisation : ";
+    var info = "Error geolocating user : ";
     switch(error.code) {
 	    case error.TIMEOUT:
-	    	info += "Timeout !";
+	    	info += "timeout";
 	    	break;
 	    case error.PERMISSION_DENIED:
-	    	info += "Vous nÕavez pas donnŽ la permission";
+	    	info += "permission denied";
 	    	break;
 	    case error.POSITION_UNAVAILABLE:
-	    	info += "La position nÕa pu tre dŽterminŽe";
+	    	info += "not able to determine position";
 	    	break;
 	    case error.UNKNOWN_ERROR:
-	    	info += "Erreur inconnue";
+	    	info += "unknown error";
 	    	break;
     }
    console.log(info);
